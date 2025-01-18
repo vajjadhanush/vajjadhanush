f6415d430646564e4b5d4a7753f941b0aa576103
vajjadhanush <vajjadhanush1@gmail.com>
2025-01-18 14:09:37 +0530

    initial commit

---
+++ addtion.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+total = num1 + num2 + num3
+print(f"The sum of {num1}, {num2}, and {num3} is: {total}")
